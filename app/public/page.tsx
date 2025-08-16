4689f8e535e6bad06a3c78fa56d78b06fc6ecd4e
nbargate <n.a.bargate@gmail.com>
2025-08-16 16:46:55 -0500

    Error Fixing

--- app/public/page.tsx
+++ app/public/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { getSupabaseBrowser } from '../lib/supabaseClient'
+import { getSupabaseBrowser } from '../../lib/supabaseClient'
 
 type Plate = {
   id: string
